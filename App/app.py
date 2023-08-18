4e047cb14fe0fab0074c9266859e6bb7407e2012
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-18 10:09:46 +0100

    imported libraries

---
+++ App/app.py
@@ -0,0 +1,5 @@
+# Importing libraries
+import streamlit as st
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
