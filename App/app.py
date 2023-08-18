a829147a74d11a6261cdb1bd66536af43fc02a73
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-18 10:11:27 +0100

    loaded the trained model

--- App/app.py
+++ App/app.py
@@ -3,3 +3,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
+
+# Loading the tranied model
+pac = PassiveAggressiveClassifier(max_iter=50)
+tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
