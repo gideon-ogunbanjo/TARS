209c2c8b8971bc5fb8ef81d29343e7a4703807b6
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-18 10:13:12 +0100

    created streamlit UI

--- App/app.py
+++ App/app.py
@@ -7,3 +7,14 @@ from sklearn.linear_model import PassiveAggressiveClassifier
 # Loading the tranied model
 pac = PassiveAggressiveClassifier(max_iter=50)
 tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+
+# Streamlit UI
+st.set_page_config(
+    page_title="TARS",
+    initial_sidebar_state="expanded",
+    layout="centered"
+)
+
+st.title("TARS - Fake News Detector")
+st.header("TARS is a machine learning model that predicts fake news. The model can be used to classify news articles as either real or fake based on the text data in the article.")
+text_input = st.text_area("Enter a news article:", "")
